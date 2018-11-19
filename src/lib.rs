9bbcd9dbe85e84cd96c5e2b9a9941af2c4e8aec1
Shion Yamashita <shioyama1118@gmail.com>
2018-11-19 15:46:47 +0900

    Moved rsh loop into main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,5 @@ extern crate nix;
 
 pub mod error;
 pub mod status;
-pub mod rsh_loop;
 pub mod command;
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
 extern crate rsh;
-use rsh::rsh_loop::*;
+
+use rsh::status::Status;
+use rsh::error::RshError;
+use rsh::command::*;
+use std::io::prelude::*;
 
 fn main() {
-    let rsh_loop = RshLoop::new();
+    println!("Welcome to rsh, minimal shell implemented with Rust.");
 
-    match rsh_loop.start() {
-        Ok(_) => println!("Exited successfully."),
+    match run() {
+        Ok(_)  => println!("Bye."),
         Err(e) => eprintln!("{}", e),
     }
+}
+
+fn run() -> Result<Status, RshError> {
+    loop {
+        print!("$ ");
+        std::io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        let _ = std::io::stdin().read_line(&mut input);
+
+        let args = input.split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>();
+
+        match rsh_execute(args) {
+            Ok(status) => match status {
+                Status::Success => continue,
+                Status::NoCommand => continue,
+                Status::Exit => return Ok(Status::Exit),
+            }
+            Err(e) => return Err(e),
+        }
+    }
+
 }
